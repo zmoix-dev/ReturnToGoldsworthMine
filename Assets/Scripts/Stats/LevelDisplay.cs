409acdd641f9e62a7d9c2fd674242bd5f7a158d8
zach-m <zachary-moix@pluralsight.com>
2023-07-16 01:30:42 -0600

    adding levels and display for levels

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Core;
+using TMPro;
+using UnityEngine;
+
+namespace RPG.Stats {
+    public class LevelDisplay : MonoBehaviour {
+    
+        BaseStats baseStats;
+        TextMeshProUGUI display;
+        
+        void Awake()
+        {
+            baseStats = GameObject.FindWithTag(UnitType.GetType(UnitType.Type.PLAYER)).GetComponent<BaseStats>();
+            display = GetComponent<TextMeshProUGUI>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            display.text = $"{baseStats.GetLevel()}";
+        }
+
+    }
+}
