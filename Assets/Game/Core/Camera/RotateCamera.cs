604d1f144b8d1efc4f871cd7dfc9543f34937092
zach-m <zachary-moix@pluralsight.com>
2023-07-22 14:59:32 -0600

    rotating camera with q+e

---
+++ Assets/Game/Core/Camera/RotateCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core.Camera {
+    public class RotateCamera : MonoBehaviour
+    {
+        [SerializeField] float rotationSpeed = 1f;
+        [SerializeField] GameObject player;
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Q)) {
+                transform.RotateAround(player.transform.position, Vector3.up, 90 * (rotationSpeed * Time.deltaTime));
+            } 
+            if (Input.GetKey(KeyCode.E)) {
+                transform.RotateAround(player.transform.position, Vector3.up, -90 * (rotationSpeed * Time.deltaTime));
+            }
+        }
+    }
+}
